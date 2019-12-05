d07bbf89b7c836dd4fd825886c6d32bddb9724ca
Marcelina Wajdzik <marcelawa20@gmail.com>
2019-12-05 19:07:41 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text> Hej swiecie co u Ciebie</Text>
+      <Text>nie wiem co ja tutj  robie</Text>
+    </View >);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
