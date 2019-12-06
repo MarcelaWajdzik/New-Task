dc93a1d365e907b79231ea6bbc73342eb19afab0
Marcelina Wajdzik <marcelawa20@gmail.com>
2019-12-06 18:12:16 +0100

    The change of structure in the component - Index.js

--- Index.js
+++ Index.js
@@ -3,40 +3,63 @@ import { StyleSheet, Text, View, } from 'react-native';
 import { Image } from 'react-native';
 
 class Index extends React.Component {
-    state = {}
+    state = {
+
+    }
 
     componentDidMount() {
-        fetch('http://xkcd.com/info.0.json')
+        return fetch('http://xkcd.com/6/info.0.json')
+            /* return fetch('http://xkcd.com/'+number +'/info.json') */
             .then((response) => response.json())
             .then((data) => this.setState({
                 newText: data.title,
+                newDay: data.day,
                 newMonth: data.month,
                 newYear: data.year,
                 newImg: data.img,
             }));
+
+
     }
-
     render() {
-        var icon = this.state.newImg
-        return (
-            < View style={styles.smallContainer} >
-                <Text>{this.state.newText} </Text>
-                <Text>{this.state.newMonth} </Text>
-                <Text>{this.state.newYear} </Text>
-                <Image source={icon} />
-            </View >)
 
+        return (
+
+            < View style={styles.parentContainer} >
+                <View style={styles.childContainer}>
+                    <Text style={styles.paragraph}>Opis: {this.state.newText} </Text>
+                    <Text style={styles.paragraphSmall}>Dodano: {this.state.newDay}/{this.state.newMonth}/{this.state.newYear} </Text>
+                </View>
+                <View style={styles.childContainer}>
+                    <Image source={{ uri: this.state.newImg }} style={{ width: 150, height: 150 }} />
+                </View>
+
+            </View >
+
+        )
     }
 }
 export default Index;
 
 const styles = StyleSheet.create({
-    smallContainer: {
-        marginTop: 10,
+    parentContainer: {
         flexDirection: "row",
+        justifyContent: 'space-around',
+        marginTop: 10,
         width: '100%',
-        backgroundColor: 'yellow',
-        padding: 20,
+        backgroundColor: 'beige',
+        padding: 10,
+
     },
+    childContainer: {
+        justifyContent: 'center',
+    },
+    paragraph: {
+        fontSize: 20,
+        color: 'red',
+    },
+    paragraphSmall: {
+        fontStyle: 'italic',
+    }
 
 });
