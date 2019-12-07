e0d6b2b21f03017839df60e149e5cabac4e9e90a
Marcelina Wajdzik <marcelawa20@gmail.com>
2019-12-07 22:07:31 +0100

    The change in  the componentDidMount

--- Index.js
+++ Index.js
@@ -11,17 +11,22 @@ class Index extends React.Component {
         for (let i = 0; i < 8; i++) {
             return fetch('http://xkcd.com/' + i + '/info.0.json')
                 .then((response) => response.json())
-                .then((data) => this.setState({
-                    newData: data,
-                }
-                ));
+                .then((data) => {
+                    this.setState({
+                        newData: newData.push(data)
+                    })
+                })
         }
+
     }
+
     render() {
 
-        this.state.newData.map(post => {
+        const posts = this.state.newData
+
+        return (posts.map(post => {
             return (
-                < View style={styles.parentContainer} >
+                < View style={styles.parentContainer} key={post.num} >
 
                     <View style={styles.childContainer}>
                         <Text style={styles.paragraph}>Opis: {post.title} </Text>
@@ -30,12 +35,16 @@ class Index extends React.Component {
                     <View style={styles.childContainer}>
                         <Image source={{ uri: post.img }} style={{ width: 150, height: 150 }} />
                     </View>
-                </View >
-            )
-        });
+                </View >)
+        }))
 
     }
 }
+
+
+
+
+
 export default Index;
 
 const styles = StyleSheet.create({
@@ -61,3 +70,17 @@ const styles = StyleSheet.create({
     }
 
 });
+
+
+/* return (
+    < View style={styles.parentContainer} key={post.num} >
+
+        <View style={styles.childContainer}>
+            <Text style={styles.paragraph}>Opis: {post.title} </Text>
+            <Text style={styles.paragraphSmall}>Dodano: {post.day}/{post.moth}/{msContentScript.year} </Text>
+        </View>
+        <View style={styles.childContainer}>
+            <Image source={{ uri: post.img }} style={{ width: 150, height: 150 }} />
+        </View>
+    </View >
+) */
